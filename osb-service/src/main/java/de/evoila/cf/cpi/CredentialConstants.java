9d6605ac4d96559f3f24ee1b7bd06155c9d4ca84
René Schollmeyer <rschollmeyer@evoila.de>
2020-04-24 16:17:52 +0200

    Revert "test(backup): test something"
    This reverts commit cfba384d52d576f63e2ec3c6be1a22164b3dd4f9.

--- osb-service/src/main/java/de/evoila/cf/cpi/CredentialConstants.java
+++ osb-service/src/main/java/de/evoila/cf/cpi/CredentialConstants.java
@@ -7,6 +7,5 @@ public class CredentialConstants {
 
     public static String ROOT_CREDENTIALS = "root_credentials";
     public static String BACKUP_CREDENTIALS = "backup_credentials";
-    public static String BACKUP_AGENT_CREDENTIALS = "backup_agent_credentials";
     public static String EXPORTER_CREDENTIALS = "exporter_credentials";
 }
